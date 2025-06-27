b01cc86451dccc14e513732dfd9bf541278360f7
Frankz Alarcon <leninalarcon7@gmail.com>
2025-06-27 10:26:21 -0500

    setup inngest for background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,20 @@
+import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter } from "../init";
 import { z } from "zod";
 
 export const appRouter = createTRPCRouter({
+    invoke: baseProcedure.input(
+        z.object({
+            text: z.string()
+        })
+    ).mutation(async ({ input }) => {
+        await inngest.send({
+            name: "test/hello.world",
+            data: {
+                email: input.text
+            }
+        })
+    }),
     createAI: baseProcedure
     .input(
         z.object({
